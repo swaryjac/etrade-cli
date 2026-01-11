97b3504a09b2849ed7bf487c722dbc4a16494bec
Jacob Swary <jacob.swary@gmail.com>
2026-01-11 12:13:28 -0800

    Get all onepct options

--- calcoptions.sh
+++ calcoptions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-option_list_file="options_input.txt"
+option_list_file="$1"
 if [ ! -f $option_list_file ]; then
   echo "file $option_list_file not found"
   exit 1
@@ -8,17 +8,14 @@ fi
 
 all_symbols=$(cat ${option_list_file} | awk '{print $2}')
 
-put_index=0
-if [[ -n $1 ]]; then
-  put_index=$1
-fi
-echo $put_index
-
-output_file=onepct_options_${put_index}.txt
+output_file=onepct_options.txt
+output_csv_file=onepct_options.csv
 
 if [ -f $output_file ]; then
   rm $output_file
 fi
+
+echo "SYMBOL,PRICE,STRIKE,PUT_BID,PCT,SPREAD" > ${output_csv_file}
 
 for quote_symbol in $all_symbols; do
 
@@ -34,21 +31,29 @@ for quote_symbol in $all_symbols; do
   one_pct_price="$(echo "scale=3; $stock_price * 0.01" | bc)"
 #  echo "10% - $one_pct_price"
 
-  put_price=$(jq --argjson put_index "$put_index" '.OptionChainResponse.OptionPair.[$put_index].Put.bid' ${option_file})
-  strike_price=$(jq --argjson put_index "$put_index" '.OptionChainResponse.OptionPair.[$put_index].Put.strikePrice' ${option_file})
+  put_price=$(jq '.OptionChainResponse.OptionPair.[0].Put.bid' ${option_file})
+  strike_price=$(jq '.OptionChainResponse.OptionPair.[0].Put.strikePrice' ${option_file})
 
-#  echo "$put_price $strike_price"
-#  echo $quote_symbol $put_price / $stock_price
-#  echo "scale=3; $put_price / $stock_price" | bc
-#  echo $one_pct_price
+  for i in {1..5}; do
+    if [ $(echo "$strike_price >= $stock_price" | bc -l) -eq 1 ]; then
+      break;
+    fi
 
-  if [ $(echo "$put_price >= $one_pct_price" | bc -l) -eq 1 ]; then
-    echo good: "$put_price $strike_price"
-    echo "${quote_symbol} ${stock_price} - strike: ${strike_price} bid: ${put_price}" >> $output_file
-  fi
+#    echo "$put_price $strike_price"
+#    echo $quote_symbol $put_price / $stock_price
+#    echo "scale=3; $put_price / $stock_price" | bc
+#    echo $one_pct_price
 
-#  if [ $(echo "$put_price < $one_pct_price" | bc -l) -eq 1 ]; then
-#    echo bad: "$put_price $strike_price"
-#    echo "${quote_symbol} ${stock_price} - strike: ${strike_price} bid: ${put_price}"
-#  fi
+    if [ $(echo "$put_price >= $one_pct_price" | bc -l) -eq 1 ]; then
+      put_pct="$(echo "scale=3; $put_price / $stock_price" | bc)"
+      price_spread="$(echo "$stock_price - $strike_price" | bc)"
+      echo good: "$put_price $strike_price $put_pct"
+      echo "${quote_symbol} ${stock_price} - strike: ${strike_price} bid: ${put_price} pct: ${put_pct} spread: ${price_spread}" >> $output_file
+      echo "${quote_symbol},${stock_price},${strike_price},${put_price},${put_pct},${price_spread}" >> $output_csv_file
+      break;
+    fi
+
+    put_price=$(jq --argjson i "$i" '.OptionChainResponse.OptionPair.[$i].Put.bid' ${option_file})
+    strike_price=$(jq --argjson i "$i" '.OptionChainResponse.OptionPair.[$i].Put.strikePrice' ${option_file})
+  done
 done
