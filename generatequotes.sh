670e51c01e3a465b5700614577ecfdcb5bbe4433
Jacob Swary <jacob.swary@gmail.com>
2026-01-11 12:13:56 -0800

    Download and prune weekly csv

--- generatequotes.sh
+++ generatequotes.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-symbol_csv_file="$1"
+definitions_file=etrade_defns.sh
+if [ ! -f ${definitions_file} ]; then
+  echo "Can't source ${definitions_file}"
+  exit 1
+fi
+
+source ${definitions_file}
+
+date_string=$(date +"%Y"-"%m"-"%d")
+all_weekly_file="weekly${date_string}.csv"
+symbol_csv_file="weekly_equities${date_string}.csv"
+
+if [ ! -f $all_weekly_file ]; then
+  weekly_csv_url="https://www.cboe.com/available_weeklys/get_csv_download/"
+
+  http_get ${weekly_csv_url} "" ${all_weekly_file}
+fi
+
+equities_header_regex="^Available.*Equity$"
+sed -n '/'"${equities_header_regex}"'/,$p' ${all_weekly_file} | sed '1d' > ${symbol_csv_file}
 
 if [ ! -f $symbol_csv_file ]; then
   echo "file doesn't exist: $symbol_csv_file"
