a56d9774351d5c0c874ab2464856ae92cb2bbadf
Jacob Swary <jacob.swary@gmail.com>
2026-02-08 12:37:27 -0800

    Calculate options for stocks in price range

--- calcoptions.sh
+++ calcoptions.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
+function is_num() {
+  if [[ "$1" =~ ^[0-9]+(\.[0-9]*)?$ ]]; then
+    return 0
+  fi
+  return 1
+}
+
 option_list_file="$1"
 if [ ! -f $option_list_file ]; then
   echo "file $option_list_file not found"
   exit 1
 fi
 
-all_symbols=$(cat ${option_list_file} | awk '{print $2}')
+if [[ $# > 1 ]] && is_num "$2"; then
+  min_price="$2"
+else
+  min_price=0
+fi
+if [[ $# > 2 ]] && is_num "$3"; then
+  max_price="$3"
+else
+  max_price=10000
+fi
+
+all_symbols=$( \
+  sort -n "${option_list_file}" \
+  | awk -v min="$min_price" -v max="$max_price" \
+      '$1 >= min && $1 <= max {print $1 "," $2}' \
+)
 
 date_string=$(date +"%Y"-"%m"-"%d")
 output_file=onepct_options${date_string}.txt
-output_csv_file=onepct_options${date_string}.csv
+output_csv_file=onepct_options${date_string}_${min_price}-${max_price}.csv
 
 if [ -f $output_file ]; then
   rm $output_file
@@ -18,7 +40,8 @@ fi
 
 echo "SYMBOL,PRICE,STRIKE,PUT_BID,PCT,SPREAD" > ${output_csv_file}
 
-for quote_symbol in $all_symbols; do
+for quote_line in $all_symbols; do
+  IFS=',' read stock_price quote_symbol <<< "${quote_line}"
 
   option_file="quotes/${quote_symbol}_opt.json"
 
@@ -26,11 +49,12 @@ for quote_symbol in $all_symbols; do
     echo "file $option_file not found"
     continue
   fi
-
-  stock_price=$(jq '.OptionChainResponse.nearPrice' ${option_file})
+  if ! jq -e 'has("OptionChainResponse")' ${option_file} > /dev/null; then
+    echo "No option detected for ${quote_symbol}"
+    continue
+  fi
 
   one_pct_price="$(echo "scale=3; $stock_price * 0.01" | bc)"
-#  echo "10% - $one_pct_price"
 
   put_price=$(jq '.OptionChainResponse.OptionPair.[0].Put.bid' ${option_file})
   strike_price=$(jq '.OptionChainResponse.OptionPair.[0].Put.strikePrice' ${option_file})
@@ -39,11 +63,6 @@ for quote_symbol in $all_symbols; do
     if [ $(echo "$strike_price >= $stock_price" | bc -l) -eq 1 ]; then
       break;
     fi
-
-#    echo "$put_price $strike_price"
-#    echo $quote_symbol $put_price / $stock_price
-#    echo "scale=3; $put_price / $stock_price" | bc
-#    echo $one_pct_price
 
     if [ $(echo "$put_price >= $one_pct_price" | bc -l) -eq 1 ]; then
       put_pct="$(echo "scale=3; $put_price / $stock_price" | bc)"
